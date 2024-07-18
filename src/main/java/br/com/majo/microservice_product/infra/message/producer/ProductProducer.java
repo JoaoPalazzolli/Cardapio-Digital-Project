600b8d59feaf5d79732560d59e7cc4deeb3da1dd
João Pedro <joaopedropalazzolli@gmail.com>
2024-07-18 18:38:30 -0300

    refactor - Foi alterado o tipo de dado de envio de Object para um Map

--- src/main/java/br/com/majo/microservice_product/infra/message/producer/ProductProducer.java
+++ src/main/java/br/com/majo/microservice_product/infra/message/producer/ProductProducer.java
@@ -1,15 +1,23 @@
 package br.com.majo.microservice_product.infra.message.producer;
 
+import br.com.majo.microservice_product.infra.util.MethodType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 @Component
 public class ProductProducer {
+
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     @Autowired
     private NewTopic newTopic;
@@ -20,14 +28,30 @@ public class ProductProducer {
     @Autowired
     private ObjectMapper objectMapper;
 
-    public void sendMessageToAddProductInCategory(Object product){
-
+    public void sendMessageToCategory(MethodType methodType, String categoryId, Object product){
         try{
-            kafkaTemplate.send(newTopic.name(), objectMapper.writeValueAsString(product));
+            Map<String, Object> data = new HashMap<>();
+            data.put("id", categoryId);
+            data.put("product", product);
+
+            kafkaTemplate.send(newTopic.name(), methodType.toString(), objectMapper.writeValueAsString(data));
         } catch (KafkaException e){
-            System.out.println("Erro producer kafka: " + e.getMessage());
+            logger.info("Kafka produce error: " + e.getMessage());
         } catch (JsonProcessingException e) {
-            System.out.println("Erro: " + e.getMessage());
+            logger.info("Json error: " + e.getMessage());
+        }
+    }
+
+    public void sendMessageToCategory(MethodType methodType, Object product){
+        try{
+            Map<String, Object> data = new HashMap<>();
+            data.put("product", product);
+
+            kafkaTemplate.send(newTopic.name(), methodType.toString(), objectMapper.writeValueAsString(data));
+        } catch (KafkaException e){
+            logger.info("Kafka produce error: " + e.getMessage());
+        } catch (JsonProcessingException e) {
+            logger.info("Json error: " + e.getMessage());
         }
     }
 }
