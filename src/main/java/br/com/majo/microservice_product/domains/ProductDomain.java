2afc950c61816fbe4750d093e4281f7450c26c14
João Pedro <joaopedropalazzolli@gmail.com>
2024-07-17 21:26:25 -0300

    feat - criado domain, dto

---
+++ src/main/java/br/com/majo/microservice_product/domains/ProductDomain.java
@@ -0,0 +1,42 @@
+package br.com.majo.microservice_product.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "product")
+public class ProductDomain implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    @Field(name = "url_image")
+    private String urlImage;
+
+    @Field(name = "sold_off")
+    private Boolean soldOff;
+
+    private Date createAt;
+}
