ca0bc5bce8c56aff713d01316b768639bbdeaa76
João Pedro <joaopedropalazzolli@gmail.com>
2024-07-18 18:47:48 -0300

    feat - Criado annotation para facilitar na configuração dos topics

---
+++ src/main/java/br/com/majo/category_microservice/infra/utils/CategoryListener.java
@@ -0,0 +1,22 @@
+package br.com.majo.category_microservice.infra.utils;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@KafkaListener
+public @interface CategoryListener {
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
+    String groupId() default "";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "topics")
+    String topics() default "${topic.product.producer.name}";
+
+}
