6bb10ba9f2162967ab619245a741208e26b39c29
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-07-19 17:44:39 -0300

    style - atualizado nome

--- src/main/java/br/com/majo/microservice_product/infra/util/MethodType.java
+++ src/main/java/br/com/majo/microservice_product/infra/util/MethodType.java
@@ -1,5 +1,5 @@
 package br.com.majo.microservice_product.infra.util;
 
 public enum MethodType {
-    CREATE, UPDATE, DELETE, UPDATE_SOLDOFF_STATUS, UPDATE_URL_IMAGE
+    CREATE, UPDATE, DELETE, UPDATE_SOLD_OUT_STATUS, UPDATE_URL_IMAGE
 }
