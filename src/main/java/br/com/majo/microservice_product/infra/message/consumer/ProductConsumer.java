7cbd1fdc904e09962f4b9103bc37c41134b1c595
João Pedro <joaopedropalazzolli@gmail.com>
2024-07-24 21:36:13 -0300

    refactor, feat - troca da localização do topic e adicionado novo consumer para upload-service

--- src/main/java/br/com/majo/microservice_product/infra/message/consumer/ProductConsumer.java
+++ src/main/java/br/com/majo/microservice_product/infra/message/consumer/ProductConsumer.java
@@ -1,11 +1,12 @@
 package br.com.majo.microservice_product.infra.message.consumer;
 
-import br.com.majo.microservice_product.controllers.ProductController;
 import br.com.majo.microservice_product.infra.util.ProductListener;
+import br.com.majo.microservice_product.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.KafkaException;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Component
@@ -14,12 +15,25 @@ public class ProductConsumer {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     @Autowired
-    private ProductController productController;
+    private ProductService productService;
 
-    @ProductListener(groupId = "${topic.product.consumer.group-id}")
-    public void consumerProducts(String message){
+    @ProductListener(topics = "${topic.category.name}")
+    public void consumerFromCategory(String message){
         try{
             logger.info(message);
+        } catch (KafkaException e){
+            logger.info("Kafka Consumer Error: " + e.getMessage());
+        }
+    }
+
+    @ProductListener(topics = "${topic.upload.name}")
+    public void consumerFromUpload(Map<String, String> data){
+        try{
+            var productId = data.get("productId");
+            var urlImage = data.get("urlImage");
+
+            productService.updateUrlImage(productId, urlImage);
+
         } catch (KafkaException e){
             logger.info("Kafka Consumer Error: " + e.getMessage());
         }
