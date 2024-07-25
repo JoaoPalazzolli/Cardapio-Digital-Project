d1eab75ff230defa5eb616be1fc6a92053f930d9
João Pedro <joaopedropalazzolli@gmail.com>
2024-07-24 22:45:51 -0300

    feat - criado um response padrão para as exception, um customize handler e uma exception personalizada

---
+++ src/main/java/br/com/majo/upload_service/infra/exceptions/handler/CustomHandlerException.java
@@ -0,0 +1,41 @@
+package br.com.majo.upload_service.infra.exceptions.handler;
+
+import br.com.majo.upload_service.infra.exceptions.ResponseException;
+import br.com.majo.upload_service.infra.exceptions.UploadException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class CustomHandlerException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> allExceptionsHandler(WebRequest request, Exception ex){
+
+        var responseException = ResponseException.builder()
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .detail(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(responseException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UploadException.class)
+    public ResponseEntity<?> uploadExceptionHandler(WebRequest request, Exception ex){
+
+        var responseException = ResponseException.builder()
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .detail(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(responseException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
