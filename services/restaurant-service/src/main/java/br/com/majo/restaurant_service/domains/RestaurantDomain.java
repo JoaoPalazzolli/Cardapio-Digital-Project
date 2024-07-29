5a5c36b0eea535088face010f3e44e497c5219bb
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-07-29 15:42:59 -0300

    feat - criado domain e dto

---
+++ services/restaurant-service/src/main/java/br/com/majo/restaurant_service/domains/RestaurantDomain.java
@@ -0,0 +1,34 @@
+package br.com.majo.restaurant_service.domains;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "restaurant")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class RestaurantDomain implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "createAt")
+    private Date createAt;
+}
---
+++ services/restaurant-service/src/main/java/br/com/majo/restaurant_service/dtos/RestaurantDTO.java
@@ -0,0 +1,24 @@
+package br.com.majo.restaurant_service.dtos;
+
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@EqualsAndHashCode(callSuper = true)
+public class RestaurantDTO extends RepresentationModel<RestaurantDTO> implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private String name;
+    private Date createAt;
+}
