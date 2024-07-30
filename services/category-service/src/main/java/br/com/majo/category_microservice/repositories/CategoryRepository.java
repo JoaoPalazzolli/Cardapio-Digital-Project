404672007bb399746073df3c0ebf938d8af6fbb0
João Pedro <joaopedropalazzolli@gmail.com>
2024-07-30 00:01:13 -0300

    feat - adicionado busca no database de todos as categorias com o id do restaurante e busca por nome e id do restaurante

--- services/category-service/src/main/java/br/com/majo/category_microservice/repositories/CategoryRepository.java
+++ services/category-service/src/main/java/br/com/majo/category_microservice/repositories/CategoryRepository.java
@@ -4,12 +4,18 @@ import br.com.majo.category_microservice.domains.CategoryDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface CategoryRepository extends MongoRepository<CategoryDomain, String> {
 
-    Optional<CategoryDomain> findByName(String name);
+    @Query(" { 'name': ?0, 'restaurantId': ?1 } ")
+    Optional<CategoryDomain> findByNameAndRestaurantId(String name, UUID restaurantId);
 
     @Query("{ 'products._id': ?0 }")
     Optional<CategoryDomain> findByProductId(String id);
+
+    @Query(" { 'restaurantId': ?0 } ")
+    List<CategoryDomain> findAllByRestaurantId(UUID restaurantId);
 }
