3a3469664cffdbbc9c3201d76d43cc0a7fb5cd12
João Pedro <joaopedropalazzolli@gmail.com>
2024-08-01 00:51:45 -0300

    refactor - habilitado cache

--- services/product-service/src/main/java/br/com/majo/microservice_product/ProductServiceApplication.java
+++ services/product-service/src/main/java/br/com/majo/microservice_product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.majo.microservice_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class ProductServiceApplication {
 	}
 
 }
+
