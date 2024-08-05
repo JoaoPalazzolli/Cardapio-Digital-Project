81fd1a22cdcc1e4c2fb732669499be58c29e1753
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-08-05 19:51:50 -0300

    feat - criado service para salvar a ultima versão dos produtos dentro do cache

---
+++ services/product-service/src/main/java/br/com/majo/productservice/services/ProductCacheService.java
@@ -0,0 +1,44 @@
+package br.com.majo.productservice.services;
+
+import br.com.majo.productservice.infra.cache.ProductCache;
+import br.com.majo.productservice.repositories.ProductCacheRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProductCacheService {
+
+    @Autowired
+    private ProductCacheRepository productCacheRepository;
+
+    public void saveLastVersion(ProductCache product){
+        productCacheRepository.save(product); // armazenando versão antiga do produto no cache
+
+        log.info("success saved product in cache");
+    }
+
+    public void saveLastVersion(String id, Object object){
+        // armazenando versão antiga do produto no cache
+
+        if (object instanceof Boolean){
+            System.out.println("TESTE");
+            productCacheRepository.save(ProductCache.builder()
+                    .id(id)
+                    .soldOut((Boolean) object)
+                    .build());
+
+            System.out.println(productCacheRepository.findAll());
+        }
+
+        if (object instanceof String){
+            productCacheRepository.save(ProductCache.builder()
+                    .id(id)
+                    .urlImage((String) object)
+                    .build());
+        }
+
+        log.info("success saved product in cache");
+    }
+}
