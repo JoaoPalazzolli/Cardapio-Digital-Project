252d600f6ea36b5a6a5b33db66f0fe4c87a7268f
João Pedro <joaopedropalazzolli@gmail.com>
2024-08-07 22:35:54 -0300

    refactor - o método para salvar a ultima versão foi modificado para ficar mais limpa

--- services/product-service/src/main/java/br/com/majo/productservice/services/ProductCacheService.java
+++ services/product-service/src/main/java/br/com/majo/productservice/services/ProductCacheService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Slf4j
 @Service
 public class ProductCacheService {
@@ -13,28 +15,25 @@ public class ProductCacheService {
     @Autowired
     private ProductCacheRepository productCacheRepository;
 
-    public void saveLastVersion(ProductCache product){
+    public void saveLastVersion(ProductCache product) {
         productCacheRepository.save(product); // armazenando versão antiga do produto no cache
 
         log.info("success saved product in cache");
     }
 
-    public void saveLastVersion(String id, Object object){
+    public void saveLastVersion(String id, Object object, UUID restaurantId) {
         // armazenando versão antiga do produto no cache
 
-        if (object instanceof Boolean){
-            productCacheRepository.save(ProductCache.builder()
-                    .id(id)
-                    .soldOut((Boolean) object)
-                    .build());
-        }
+        var lastProduct = ProductCache.builder()
+                .id(id)
+                .restaurantId(restaurantId)
+                .build();
 
-        if (object instanceof String){
-            productCacheRepository.save(ProductCache.builder()
-                    .id(id)
-                    .urlImage((String) object)
-                    .build());
-        }
+        if (object instanceof Boolean) lastProduct.setSoldOut((Boolean) object);
+
+        if (object instanceof String) lastProduct.setUrlImage((String) object);
+
+        productCacheRepository.save(lastProduct);
 
         log.info("success saved product in cache");
     }
