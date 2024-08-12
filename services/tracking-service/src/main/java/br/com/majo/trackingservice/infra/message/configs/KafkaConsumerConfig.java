b1d0aaf7f070503ad72a68933d336f1eaf3f4671
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-08-12 16:13:25 -0300

    feat - criado configs do kafka

---
+++ services/tracking-service/src/main/java/br/com/majo/trackingservice/infra/message/configs/KafkaConsumerConfig.java
@@ -0,0 +1,70 @@
+package br.com.majo.trackingservice.infra.message.configs;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Value("${topic.tracking.group-id}")
+    private String groupId;
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String offset;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Bean
+    ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.put(GROUP_ID_CONFIG, groupId);
+        props.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(AUTO_OFFSET_RESET_CONFIG, offset);
+        props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new StringDeserializer());
+    }
+
+    @Bean
+    DefaultErrorHandler errorHandler(){
+        return new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(kafkaTemplate),
+                new FixedBackOff(1000L, 3L)
+        );
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory());
+        factory.setCommonErrorHandler(errorHandler());
+        factory.setRecordMessageConverter(new JsonMessageConverter());
+        return factory;
+    }
+}
