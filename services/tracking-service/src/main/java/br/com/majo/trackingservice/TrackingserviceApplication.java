590276ecd643056cf3c30bffc9dd7d01be38e221
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-08-12 16:07:49 -0300

    Criado ambiente para o tracking service

---
+++ services/tracking-service/src/main/java/br/com/majo/trackingservice/TrackingserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.majo.trackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingserviceApplication.class, args);
+	}
+
+}
---
+++ services/tracking-service/src/test/java/br/com/majo/trackingservice/TrackingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.majo.trackingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
