033160aa47036fd16e09f65a6938d324666a9299
João Pedro <joaopedropalazzolli@gmail.com>
2024-08-14 22:36:56 -0300

    refactor - envia mensagem criando um tracking da request e retorna o id dele no body

--- services/upload-service/src/main/java/br/com/majo/uploadservice/services/UploadService.java
+++ services/upload-service/src/main/java/br/com/majo/uploadservice/services/UploadService.java
@@ -3,6 +3,7 @@ package br.com.majo.uploadservice.services;
 import br.com.majo.uploadservice.infra.message.producer.UploadProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,12 +21,19 @@ public class UploadService {
     private UploadProducer producer;
 
     public ResponseEntity<?> uploadProductImage(String productId, UUID restaurantId, MultipartFile image) {
-        String urlImage = abstractLocalUploadImageService.upload(image);
+        String imageUrl = abstractLocalUploadImageService.upload(image);
 
-        producer.sendMessageToProduct(productId, restaurantId, urlImage);
-        log.info("Image upload successful -> {}", urlImage);
+        var trackingId = generateTrackingId();
 
-        return ResponseEntity.noContent().build();
+        producer.sendMessageToTracking("PENDING", "updating the product image url", trackingId);
+        producer.sendMessageToProduct(productId, restaurantId, imageUrl, trackingId);
+        log.info("Image upload successful -> {}", imageUrl);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(trackingId);
+    }
+
+    private String generateTrackingId(){
+        return UUID.randomUUID().toString();
     }
 
 }
