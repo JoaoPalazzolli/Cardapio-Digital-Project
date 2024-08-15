bc57b122252ec0952dc966234ab4db05ae5ccab6
João Pedro <joaopedropalazzolli@gmail.com>
2024-08-14 23:27:11 -0300

    refactor - adicionado o envio de mensagem de erro dentro do catch e não mais no orElseThrow e adicionado também o envio do status de sucesso para o tracking

--- services/category-service/src/main/java/br/com/majo/categoryservice/infra/external/services/ProductService.java
+++ services/category-service/src/main/java/br/com/majo/categoryservice/infra/external/services/ProductService.java
@@ -5,6 +5,7 @@ import br.com.majo.categoryservice.infra.external.dtos.ProductDTO;
 import br.com.majo.categoryservice.infra.message.producer.CategoryProducer;
 import br.com.majo.categoryservice.infra.utils.RollbackMethod;
 import br.com.majo.categoryservice.infra.utils.StatusMessage;
+import br.com.majo.categoryservice.infra.utils.TrackingStatus;
 import br.com.majo.categoryservice.repositories.CategoryRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +25,10 @@ public class ProductService {
     @Autowired
     private CategoryProducer producer;
 
-    public void addProductInCategory(String categoryId, ProductDTO productDTO, UUID restaurantId){
+    public void addProductInCategory(String categoryId, ProductDTO productDTO, UUID restaurantId, String trackingId){
         try {
             var category = categoryRepository.findByIdAndRestaurantId(categoryId, restaurantId)
-                    .orElseThrow(() -> {
-                        producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.CREATE, productDTO.getId());
-                        return new CategoryNotFoundException("Category not found");
-                    });
+                    .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
 
             if (category.getProducts() == null){
                 category.setProducts(Collections.singletonList(productDTO));
@@ -42,19 +40,19 @@ public class ProductService {
 
             producer.sendMessageToProduct(StatusMessage.SUCCESS, String
                     .format("product success added to category. (product id: (%s))", productDTO.getId()));
+            producer.sendMessageToTracking(TrackingStatus.PUBLISHED.toString(), trackingId);
             log.info("product success added to category. (product id: ({}))", productDTO.getId());
         } catch (KafkaException | CategoryNotFoundException e){
+            producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
+            producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.CREATE, productDTO.getId());
             log.info("Error -> {}", e.getMessage());
         }
     }
 
-    public void updateProductInCategory(ProductDTO productDTO, UUID restaurantId){
+    public void updateProductInCategory(ProductDTO productDTO, UUID restaurantId, String trackingId){
         try {
             var category = categoryRepository.findByProductIdAndRestaurantId(productDTO.getId(), restaurantId)
-                    .orElseThrow(() -> {
-                        producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE, productDTO.getId());
-                        return new CategoryNotFoundException("Category not found");
-                    });
+                    .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
 
             var product = category.getProducts().stream().filter(x -> x.getId().equals(productDTO.getId())).toList().get(0);
             var indexProduct = category.getProducts().indexOf(product);
@@ -66,20 +64,20 @@ public class ProductService {
 
             producer.sendMessageToProduct(StatusMessage.SUCCESS, String
                     .format("success updated product in category. (product id: (%s))", productDTO.getId()));
+            producer.sendMessageToTracking(TrackingStatus.PUBLISHED.toString(), trackingId);
             log.info("success updated product in category. (product id: ({}))", productDTO.getId());
         } catch (KafkaException | CategoryNotFoundException e){
+            producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
+            producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE, productDTO.getId());
             log.info("Error -> {}", e.getMessage());
         }
 
     }
 
-    public void deleteProductInCategory(ProductDTO productDTO, UUID restaurantId){
+    public void deleteProductInCategory(ProductDTO productDTO, UUID restaurantId, String trackingId){
         try {
             var category = categoryRepository.findByProductIdAndRestaurantId(productDTO.getId(), restaurantId)
-                    .orElseThrow(() -> {
-                        producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.DELETE, productDTO.getId());
-                        return new CategoryNotFoundException("Category not found");
-                    });
+                    .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
 
             category.getProducts().removeIf(x -> x.getId().equals(productDTO.getId()));
 
@@ -87,20 +85,20 @@ public class ProductService {
 
             producer.sendMessageToProduct(StatusMessage.SUCCESS, String
                     .format("success deleted product in category. (product id: (%s))", productDTO.getId()));
+            producer.sendMessageToTracking(TrackingStatus.PUBLISHED.toString(), trackingId);
             log.info("success deleted product in category. (product id: ({}))", productDTO.getId());
         } catch (KafkaException | CategoryNotFoundException e){
+            producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
+            producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.DELETE, productDTO.getId());
             log.info("Error -> {}", e.getMessage());
         }
 
     }
 
-    public void updateSoldOutStatusInCategory(String productId, boolean soldOut, UUID restaurantId){
+    public void updateSoldOutStatusInCategory(String productId, boolean soldOut, UUID restaurantId, String trackingId){
         try {
             var category = categoryRepository.findByProductIdAndRestaurantId(productId, restaurantId)
-                    .orElseThrow(() -> {
-                        producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE_SOLD_OUT_STATUS, productId);
-                        return new CategoryNotFoundException("Category not found");
-                    });
+                    .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
 
             var product = category.getProducts().stream().filter(x -> x.getId().equals(productId)).toList().get(0);
 
@@ -110,46 +108,47 @@ public class ProductService {
 
             producer.sendMessageToProduct(StatusMessage.SUCCESS, String
                     .format("sold out status has been updated successfully. (product id: (%s))", productId));
+            producer.sendMessageToTracking(TrackingStatus.PUBLISHED.toString(), trackingId);
             log.info("sold out status has been updated successfully. (product id: ({}))", productId);
         } catch (KafkaException | CategoryNotFoundException e){
+            producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
+            producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE_SOLD_OUT_STATUS, productId);
             log.info("Error -> {}", e.getMessage());
         }
     }
 
-    public void updateUrlImageInCategory(String productId, String urlImage, UUID restaurantId) {
+    public void updateImageUrlInCategory(String productId, String imageUrl, UUID restaurantId, String trackingId) {
         try {
             var category = categoryRepository.findByProductIdAndRestaurantId(productId, restaurantId)
-                    .orElseThrow(() -> {
-                        producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE_URL_IMAGE, productId);
-                        return new CategoryNotFoundException("Category not found");
-                    });
+                    .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
 
             var product = category.getProducts().stream().filter(x -> x.getId().equals(productId)).toList().get(0);
 
-            product.setUrlImage(urlImage);
+            product.setImageUrl(imageUrl);
 
             categoryRepository.save(category);
 
             producer.sendMessageToProduct(StatusMessage.SUCCESS, String
-                    .format("url image has been updated successfully. (product id: (%s))", productId));
-            log.info("url image has been updated successfully. (product id: ({}))", productId);
+                    .format("image url has been updated successfully. (product id: (%s))", productId));
+            producer.sendMessageToTracking(TrackingStatus.PUBLISHED.toString(), trackingId);
+            log.info("image url has been updated successfully. (product id: ({}))", productId);
         } catch (KafkaException | CategoryNotFoundException e){
+            producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
+            producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE_URL_IMAGE, productId);
             log.info("Error -> {}", e.getMessage());
         }
     }
 
-    public void updateProductCategory(String categoryId, ProductDTO productDTO, UUID restaurantId){
+    public void updateProductCategory(String categoryId, ProductDTO productDTO, UUID restaurantId, String trackingId){
         try {
             var sourceCategory = categoryRepository.findByProductIdAndRestaurantId(productDTO.getId(), restaurantId)
-                    .orElseThrow(() -> {
-                        producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE_CATEGORY_ID, productDTO.getId());
-                        return new CategoryNotFoundException("Category not found");
-                    });
+                    .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
 
             sourceCategory.getProducts().removeIf(x -> x.getId().equals(productDTO.getId()));
 
             var targetCategory = categoryRepository.findByIdAndRestaurantId(categoryId, restaurantId)
                     .orElseThrow(() -> {
+                        producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
                         producer.sendMessageToProduct(StatusMessage.FAILED, "category not found");
                         return new CategoryNotFoundException("Category not found");
                     });
@@ -165,8 +164,11 @@ public class ProductService {
 
             producer.sendMessageToProduct(StatusMessage.SUCCESS, String
                     .format("product category has been updated successfully. (product id: (%s))", productDTO.getId()));
+            producer.sendMessageToTracking(TrackingStatus.PUBLISHED.toString(), trackingId);
             log.info("product category has been updated successfully. (product id: ({}))", productDTO.getId());
         } catch (KafkaException | CategoryNotFoundException e){
+            producer.sendMessageToTracking(TrackingStatus.FAILED.toString(), trackingId);
+            producer.sendMessageToProduct(StatusMessage.FAILED, RollbackMethod.UPDATE_CATEGORY_ID, productDTO.getId());
             log.info("Error -> {}", e.getMessage());
         }
     }
